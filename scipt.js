9d4e9cfec784ccbb22870d77ecc5ecce0753146a
rayhaynes504 <twotone504@gmail.com>
2022-04-11 16:35:23 -0500

    Complete Kata

--- scipt.js
+++ scipt.js
@@ -2,7 +2,7 @@ function lcm(...numbers) {
 	if (numbers.length === 0) return 1;
 	if (numbers.includes(0)) return 0;
 	let possibilities = [];
-    // let bigStopNumber = numbers.reduce((previousValue, currentValue) => previousValue * currentValue, 1)
+    let bigStopNumber = numbers.reduce((previousValue, currentValue) => previousValue * currentValue, 1)
 	for (let j = 0; j < numbers.length; j++) {
         
 		for (let i = 0; i <= bigStopNumber; i++) {
